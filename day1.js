c907118081fe1199eb2118c10dfcf9da975d865c
jawinga <cristianharders@gmail.com>
2024-11-06 12:00:37 +0100

    Update day1.js

--- day1.js
+++ day1.js
@@ -3,15 +3,10 @@
  */
 
 const createHelloWorld = function () {
-  return function (...args) {
+  return function () {
     console.log("Hello World");
   };
 };
 
 const f = createHelloWorld();
 f();
-
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
